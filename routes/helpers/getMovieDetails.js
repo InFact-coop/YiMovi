e53590fdca54dbd54e56130db84eaab58feb0e59
emily <limeyb7@gmail.com>
2017-05-19 21:59:55 +0100

    fixes issues raised in #19

--- routes/helpers/getMovieDetails.js
+++ routes/helpers/getMovieDetails.js
@@ -7,39 +7,21 @@ const Director = keystone.list('Director');
 const Theme = keystone.list('Theme');
 const Genre = keystone.list('Genre');
 
-let movieDetails;
-
-const getDirector = (cb) => {
-
-  Director.model.findOne({ _id: movieDetails.director, }).exec((err, director) => {
-    if (err) return cb(err);
-    return cb(null, director);
-  });
-};
-
-const getThemes = (cb) => {
-
-  Theme.model.find({ _id: { $in: movieDetails.themes, }, }).exec((err, themes) => {
-    if (err) return cb(err);
-    return cb(null, themes);
-  });
-};
-
-const getGenres = (cb) => {
-
-  Genre.model.find({ _id: { $in: movieDetails.genre, }, }).exec((err, genres) => {
-    if (err) return cb(err);
-    return cb(null, genres);
-  });
-};
-
 module.exports = (movie, cb) => {
 
-  movieDetails = movie;
+  const getDirector = (cb) =>
+    Director.model.findOne({ _id: movie.director, }).exec(cb);
+
+  const getThemes = (cb) =>
+    Theme.model.find({ _id: { $in: movie.themes, }, }).exec(cb);
+
+  const getGenres = (cb) =>
+    Genre.model.find({ _id: { $in: movie.genre, }, }).exec(cb);
 
   async.parallel([ getDirector, getThemes, getGenres, ], (err, result) => {
 
     if (err) return cb(err);
+
     return cb(null, {
       director: result[0],
       themes: result[1],
--- routes/helpers/getMoviesBy.js
+++ routes/helpers/getMoviesBy.js
@@ -1,10 +1,8 @@
 const keystone = require('keystone');
 const Movie = keystone.list('Movie');
 
-module.exports = (field, { id, }, callback) => {
+module.exports = (field, { id, }, cb) => {
 
-  Movie.model.find({ [field]: { _id: id, }, }).exec((err, results) => {
-    if (err) return callback(err);
-    callback(null, results);
-  });
+  Movie.model.find({ [field]: { _id: id, }, }).exec(cb);
+
 };
