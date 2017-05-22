60b34960997c2fdffd0d846cd84d8c0a39beddcf
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-05-22 11:38:40 +0100

    removes comments
    related #16

--- copy_tachyons.js
+++ copy_tachyons.js
@@ -8,7 +8,3 @@ fs.writeFileSync(
   fs.readFileSync(tachyonsPath, 'utf8'),
   'utf8'
 )
-
-// 
-// /*"heroku_deploy_master": "npm run deploy && git merge master && git checkout -B deploy && git push heroku deploy:master",*/
-// "deploy": "git checkout -B deploy && git add -f public/tachyons.min.css && git commit -m \"deploy\""
