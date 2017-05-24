59c789f12e1144591b64a552eb9036c3540412f1
emily <limeyb7@gmail.com>
2017-05-24 16:18:45 +0100

    tidy up menu js file related #39

--- templates/js/menu.js
+++ templates/js/menu.js
@@ -1,32 +1,38 @@
-var menuPoints = document.querySelectorAll('.menuPoint');
+(function() {
 
-function selectPoint(e) {
-  toggleDropDown();
-  var point = e.target;
-  menuPoints.forEach(function(menuPoint) {
-    if (menuPoint.textContent === point.textContent) {
-      return point.className = point.className + ' bb';
+  function toggleDropDown() {
+    var dropDown = document.querySelector('#drop-down');
+    var dropDownClasses = dropDown.className.split(' ');
+    if (dropDownClasses.indexOf('dn') < 0) {
+      dropDownClasses = dropDownClasses.concat([ 'dn', ]);
+      dropDown.className = dropDownClasses.join(' ');
+    } else {
+      dropDown.className = dropDownClasses.filter(function(className) {
+        return className != 'dn';
+      }).join(' ');
     }
-    menuPoint.className = point.className.replace(/\sbb/g, '');
-  });
-}
-
-menuPoints.forEach(function(point) {
-  point.addEventListener('click', selectPoint);
-});
-
-function toggleDropDown() {
-  var dropDown = document.querySelector('#drop-down');
-  var dropDownClasses = dropDown.className.split(' ');
-  if (dropDownClasses.indexOf('dn') < 0) {
-    dropDownClasses = dropDownClasses.concat([ 'dn', ]);
-    dropDown.className = dropDownClasses.join(' ');
-  } else {
-    dropDown.className = dropDownClasses.filter(function(className) {
-      return className != 'dn';
-    }).join(' ');
   }
-}
 
-var mobileMenu = document.querySelector('#menu-mobile');
-mobileMenu.addEventListener('click', toggleDropDown);
+  function menuPointClick(e) {
+    toggleDropDown();
+    var point = e.target;
+    menuPoints.forEach(function(menuPoint) {
+      if (menuPoint.textContent === point.textContent) {
+        return point.className = point.className + ' bb';
+      }
+      menuPoint.className = point.className.replace(/\sbb/g, '');
+    });
+  }
+
+  var menuPoints = document.querySelectorAll('.menuPoint');
+
+  menuPoints.forEach(function(point) {
+    point.addEventListener('click', menuPointClick);
+  });
+
+  var mobileMenu = document.querySelector('#menu-mobile');
+
+  if (mobileMenu) {
+    mobileMenu.addEventListener('click', toggleDropDown);
+  }
+})();
