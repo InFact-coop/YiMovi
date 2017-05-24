1e9a257d054dccce52462e36f35d6d8064cea557
emily <limeyb7@gmail.com>
2017-05-24 15:59:02 +0100

    adapts movie profile menu for mobile screen related #39

--- templates/js/menu.js
+++ templates/js/menu.js
@@ -1,6 +1,7 @@
 var menuPoints = document.querySelectorAll('.menuPoint');
 
 function selectPoint(e) {
+  toggleDropDown();
   var point = e.target;
   menuPoints.forEach(function(menuPoint) {
     if (menuPoint.textContent === point.textContent) {
@@ -13,3 +14,19 @@ function selectPoint(e) {
 menuPoints.forEach(function(point) {
   point.addEventListener('click', selectPoint);
 });
+
+function toggleDropDown() {
+  var dropDown = document.querySelector('#drop-down');
+  var dropDownClasses = dropDown.className.split(' ');
+  if (dropDownClasses.indexOf('dn') < 0) {
+    dropDownClasses = dropDownClasses.concat([ 'dn', ]);
+    dropDown.className = dropDownClasses.join(' ');
+  } else {
+    dropDown.className = dropDownClasses.filter(function(className) {
+      return className != 'dn';
+    }).join(' ');
+  }
+}
+
+var mobileMenu = document.querySelector('#menu-mobile');
+mobileMenu.addEventListener('click', toggleDropDown);
