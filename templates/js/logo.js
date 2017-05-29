7bb358b719743bf3b39864ae134946e0a4569be9
emily <limeyb7@gmail.com>
2017-05-29 13:02:13 +0100

    fixes logo colour changing related #48

--- templates/js/logo.js
+++ templates/js/logo.js
@@ -1,22 +1,17 @@
 (function setLogoColor() {
 
-  var blue = 'RGB(36,117,120)';
-  var purple = 'RGB(129,59,149)';
-  var yellow = 'RGB(215,144,40)';
-  var green = 'RGB(72,141,65)';
-
   function getColor(view) {
     switch(view) {
     case 'movies':
-      return blue;
+      return 'svg-fill--blue';
     case 'themes':
-      return purple;
+      return 'svg-fill--light-purple';
     case 'directors':
-      return yellow;
+      return 'svg-fill--green';
     case 'genres':
-      return green;
+      return 'svg-fill--yellow';
     default:
-      return;
+      return 'st0';
     }
   }
 
@@ -24,7 +19,11 @@
   var view = url.split('/')[1];
   var logoParts = document.querySelectorAll('.st0');
   logoParts.forEach(function(part) {
-    part.style.fill = getColor(view);
+
+    if (!part.classList) return;
+
+    part.classList.remove('st0');
+    part.classList.add(getColor(view));
   });
 
 })();
