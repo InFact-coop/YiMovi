5c1b8e13fb06af10956f49bccfe3b5e4e8fca74a
emily <limeyb7@gmail.com>
2017-05-29 16:01:24 +0100

    top 4 themes included on homepage view related #31

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,7 +1,24 @@
 const keystone = require('keystone');
+const Theme = keystone.list('Theme');
 
 exports = module.exports = (req, res) => {
+
   const view = new keystone.View(req, res);
+
+  view.on('init', next => {
+    const locals = res.locals;
+    locals.themes = [];
+
+    Theme.model.find().exec((err, themes) => {
+
+      if (err) return next(err);
+
+      const sliced = themes.slice(0, 4);
+
+      locals.themes = sliced;
+      next();
+    });
+  });
 
   view.render('index');
 };
