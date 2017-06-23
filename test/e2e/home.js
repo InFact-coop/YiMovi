3feb36561a2442556bbdff626fee9d24dec17026
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-06-23 15:55:53 +0100

    adds server startup / stop to nightwatch
    related #111

--- test/e2e/home.js
+++ test/e2e/home.js
@@ -1,12 +1,15 @@
 var config = require('../../nightwatch.conf.js');
+const { start, stop } = require('../../server.js')
 
 module.exports = {
+  before: (_, done) => start(done),
+  after: stop,
   'Home': function(browser) {
     browser
-      .url(config.URL + '/')
+      .url(config.url + '/')
       .waitForElementVisible('body')
-      // .assert.title('Should Fail')
       .saveScreenshot(config.imgpath(browser) + 'home.png')
+      .pause(2000)
       .end();
   },
 };
