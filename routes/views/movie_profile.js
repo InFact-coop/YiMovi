c6f185a16032199fc5b46bfdce1826d48b61734d
emily <limeyb7@gmail.com>
2017-06-23 12:11:01 +0100

    move keystone utils to routes/index.js so that it's available in all views related #122

--- routes/views/movie_profile.js
+++ routes/views/movie_profile.js
@@ -13,7 +13,6 @@ exports = module.exports = (req, res) => {
     locals.director = {};
     locals.genres = [];
     locals.themes = [];
-    locals.utils = require('keystone-utils');
 
     Movie.model.findOne({ key: req.params.name, })
       .populate('director')
