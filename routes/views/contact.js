d91c1e03e0b961c094702654baf630ace4a30d14
Nick Field <heyimnick@live.com>
2017-06-23 15:53:49 +0100

    add alert styling
    related #93

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -4,7 +4,12 @@ exports = module.exports = (req, res) => {
 
   const view = new keystone.View(req, res);
   view.on('init', next => {
-    res.locals.hide_footer = true;
+
+    const locals = res.locals;
+
+    locals.hide_footer = true;
+    locals.email = process.env.EMAIL || 'none@none.com';
+
     next();
   });
 
--- routes/views/index.js
+++ routes/views/index.js
@@ -9,6 +9,11 @@ exports = module.exports = (req, res) => {
     const locals = res.locals;
     locals.themes = [];
 
+    // Check redirect query after sending mail
+    if (req.query.mailsent) {
+      req.flash('success', "Thank you for contacting us. We'll be in touch soon");
+    }
+
     Theme.model.find().limit(8).sort('sortOrder').exec((err, themes) => {
 
       if (err) return next(err);
