836ae5e395059bdb5a8593d88403d48198249213
Nick Field <heyimnick@live.com>
2017-06-23 15:13:04 +0100

    add contact form styles

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -3,6 +3,10 @@ const keystone = require('keystone');
 exports = module.exports = (req, res) => {
 
   const view = new keystone.View(req, res);
+  view.on('init', next => {
+    res.locals.hide_footer = true;
+    next();
+  });
 
   view.render('contact');
 };
