e5a04acd47c293d4e4c826e89772b1cf4e42f6cf
emily <limeyb7@gmail.com>
2017-06-27 10:21:56 +0100

    creates helper function that takes mongo DB results and transforms them with localized data only based on current locale related #102

---
+++ routes/helpers/localize_results.js
@@ -0,0 +1,24 @@
+module.exports = (locale, dbResults) => {
+
+  // if currentLocale === defaultLocale, do nothing
+  // english is default
+  if (locale === 'en') return dbResults;
+
+  // keynames in DB that contain translatable content follow pattern 'name___locale'
+  const separator = '___';
+  const translatableKeyReg = /\_{3}.+$/;
+
+  // gets array of translatable values from DB results
+  const translatableFields = Object.keys(dbResults._doc)
+    .filter(key => translatableKeyReg.test(key))
+    .map(filteredKey => filteredKey.replace(translatableKeyReg, ''));
+
+  // create new results object with relevant data only
+  return Object.keys(dbResults._doc)
+    .filter(key => {
+      const [ type, loc, ] = key.split(separator);
+      return translatableFields.indexOf(type) < 0 || loc === locale;
+    }) // change key name so that '___LOCALE' part is discarded
+    .reduce((a, b) =>
+      Object.assign({}, a, { [b.replace(translatableKeyReg, '')] : dbResults[b], }), {});
+};
