958a887e219b6a6ecc0f35349e0d5cd631d47bb9
emily <limeyb7@gmail.com>
2017-06-27 12:42:10 +0100

    refactor localise function to include type checking related #102 and #147

--- routes/helpers/localize_results.js
+++ routes/helpers/localize_results.js
@@ -1,24 +1,35 @@
-module.exports = (locale, dbResults) => {
-
-  // if currentLocale === defaultLocale, do nothing
-  // english is default
-  if (locale === 'en') return dbResults;
+const localize =  (locale, dbResultsObj) => {
 
   // keynames in DB that contain translatable content follow pattern 'name___locale'
   const separator = '___';
   const translatableKeyReg = /\_{3}.+$/;
 
   // gets array of translatable values from DB results
-  const translatableFields = Object.keys(dbResults)
+  const translatableFields = Object.keys(dbResultsObj)
     .filter(key => translatableKeyReg.test(key))
     .map(filteredKey => filteredKey.replace(translatableKeyReg, ''));
 
   // create new results object with relevant data only
-  return Object.keys(dbResults)
+  return Object.keys(dbResultsObj)
     .filter(key => {
       const [ type, loc, ] = key.split(separator);
       return translatableFields.indexOf(type) < 0 || loc === locale;
     }) // change key name so that '___LOCALE' part is discarded
     .reduce((a, b) =>
-      Object.assign({}, a, { [b.replace(translatableKeyReg, '')] : dbResults[b], }), {});
+      Object.assign({}, a, { [b.replace(translatableKeyReg, '')] : dbResultsObj[b], }), {});
+};
+
+module.exports = (locale, dbResults) => {
+
+  // if currentLocale === defaultLocale, do nothing
+  // english is default
+  if (locale === 'en') return dbResults;
+
+  if (dbResults instanceof Array) {
+    return dbResults.map(dbResult => localize(locale, dbResult));
+  }
+
+  // if not array, pass in _doc object
+  return localize(dbResults._doc);
+
 };
