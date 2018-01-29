c3a4ae2a0229bc8ff5eb2b63b86f785c5d38a05a
Max Gerber <mgerber94@gmail.com>
2018-01-29 16:59:56 +0000

    Implemented sticky navbar feature #186

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "sourceType": "module"
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: 'module',
   },
-  "rules": {
-    "array-bracket-spacing": ["error", "always"],
-    "comma-dangle" : [ "error", "always" ],
-    "indent": [ "error", 2, ],
-    "linebreak-style": [ "error", "unix", ],
-    "object-curly-spacing": ["error", "always"],
-    "quotes": [ "error", "single", ],
-    "semi": [ "error", "always", ],
-    "space-in-parens": ["error", "never"],
-  }
+  rules: {
+    'array-bracket-spacing': [ 'error', 'always', ],
+    'comma-dangle': [ 'error', 'always', ],
+    indent: [ 'error', 2, ],
+    'linebreak-style': [ 'error', 'unix', ],
+    'object-curly-spacing': [ 'error', 'always', ],
+    quotes: [ 'error', 'single', ],
+    semi: [ 'error', 'always', ],
+    'space-in-parens': [ 'error', 'never', ],
+  },
 };
--- routes/views/theme_profile.js
+++ routes/views/theme_profile.js
@@ -5,9 +5,7 @@ const Theme = keystone.list('Theme');
 const getMoviesBy = require('./../helpers/getMoviesBy.js');
 const { localizeResults, } = require('../helpers/localize_results.js');
 
-
 exports = module.exports = (req, res) => {
-
   const view = new keystone.View(req, res);
 
   view.on('init', next => {
@@ -16,7 +14,6 @@ exports = module.exports = (req, res) => {
     locals.movies = [];
 
     Theme.model.findOne({ key: req.params.name, }).exec((err, theme) => {
-
       if (err || !theme) {
         res.status(404).render('errors/404');
         return;
@@ -27,7 +24,6 @@ exports = module.exports = (req, res) => {
       locals.title = res.__('theme_profile.page_title', locals.theme.name);
 
       getMoviesBy('themes', theme, (moviesErr, movies) => {
-
         if (moviesErr) return next(moviesErr);
 
         locals.movies = locals.movies.concat(movies || []);
