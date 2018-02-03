1bd0ccf802e6806e821d388d0c44ebbd6c061e50
Max Gerber <mgerber94@gmail.com>
2018-02-03 18:56:16 +0000

    fixed title issue in english lists #218

--- routes/views/director_profile.js
+++ routes/views/director_profile.js
@@ -4,7 +4,6 @@ const getMoviesBy = require('./../helpers/getMoviesBy.js');
 const { localizeResults, } = require('../helpers/localize_results.js');
 
 exports = module.exports = (req, res) => {
-
   const view = new keystone.View(req, res);
 
   view.on('init', next => {
@@ -13,25 +12,22 @@ exports = module.exports = (req, res) => {
     locals.director = {};
     locals.movies = [];
 
-    Director.model
-      .findOne({ key: req.params.name, })
-      .exec((err, director) => {
+    Director.model.findOne({ key: req.params.name, }).exec((err, director) => {
+      if (err || !director) {
+        res.status(404).render('errors/404');
+        return;
+      }
 
-        if (err || !director) {
-          res.status(404).render('errors/404');
-          return;
-        }
+      locals.director = localizeResults(locals.locale, director);
 
-        locals.director = localizeResults(locals.locale, director);
+      locals.title = res.__(locals.director.name, locals.director.name_chn);
 
-        locals.title = res.__('director_profile.page_title', locals.director.name, locals.director.name_chn);
-
-        getMoviesBy('director', director, (moviesErr, movies) => {
-          if (moviesErr) return next(moviesErr);
-          locals.movies = locals.movies.concat(movies || []);
-          next();
-        });
+      getMoviesBy('director', director, (moviesErr, movies) => {
+        if (moviesErr) return next(moviesErr);
+        locals.movies = locals.movies.concat(movies || []);
+        next();
       });
+    });
   });
 
   view.render('director_profile');
